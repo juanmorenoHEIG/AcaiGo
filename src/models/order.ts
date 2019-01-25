a26d683dff4b851494ac3c1ecaa56fd2697d3e0a
juanmorenoHEIG <juan.morenoizquierdo@heig-vd.ch>
2019-01-25 15:15:57 +0100

    markers

--- src/models/order.ts
+++ src/models/order.ts
@@ -3,10 +3,10 @@ import { pickupPlaceResponse } from './pickupPlace';
 
 
 export class OrderResponse {
-  pickup_place: pickupPlaceResponse[];
+  pickup_place: pickupPlaceResponse;
   _id: String;
   pickup_date: Date;
-  state: String;
+  state: string;
   orderLines: OrderLinesResponse[];
 
   constructor(){
